9fcb125fd3189736f9c715f788da37e11b8f4f06
Uladzimir Varzou <uv@bluevisby.com>
2023-05-13 18:21:48 +0300

    add DomainTypes.fs

---
+++ FSharpApp/DomainTypes.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpApp
+
+open System
+
+module DomainTypes =
+
+    type SuccessfulWithdrawal =
+        { Amount : decimal
+          Balance : decimal }
+        
+    type FailedWithdrawal =
+        { Amount : decimal
+          Balance : decimal
+          IsOverdraft : bool }
+        
+    type WithdrawalResult =
+        | Success of SuccessfulWithdrawal
+        | InsufficientFunds of FailedWithdrawal
+        | CardExpired of DateTime
+        | UndisclosedFailure
---
+++ FSharpApp/Source.fs
@@ -0,0 +1,8 @@
+﻿module FSharpApp.Source
+
+let names = ["Peter"; "Julia"; "Xi"]
+let getGreeting name = $"Hello, {name}!"
+
+names
+|> List.map getGreeting
+|> List.iter(fun greeting -> printfn $"{greeting} Enjoy your F#")
